1ca014190705e0caa99648376df0e557488ebb58
Renato <renato.gfteixeira@outlook.com>
2023-07-17 21:44:07 -0300

    add `lint:check` and `lint:fix` scripts

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # tabnews-clone
+
 https://www.tabnews.com.br community clone developed from scratch as part of the https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Se você me ama, compra uma coquinha 😊</h1>
+  return <h1>Se você me ama, compra uma coquinha 😊</h1>;
+}
+
+function teste1() {
+  console.log();
 }
 
 export default Home;
